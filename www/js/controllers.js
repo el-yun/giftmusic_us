3c50f18f917f2ed91c768f9a9665234f6fd05e47
yoonjihwan <gm1602759@gabiasafe.com>
2016-12-12 09:02:03 +0900

    코드 정리
    정리

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,34 +14,13 @@ angular.module('starter.controllers', ['ionic', 'ionic-audio'])
 	$scope.dynamicTrack = {};
 
 	$scope.tracks = TrackList.all();
-	$scope.checkTrack = [];
 
 	$scope.stopPlayback = function() {
-		if(NextIndex == 0) MediaManager.stop();
-		else {
-			$scope.playTrack(NextIndex);
-			NextIndex++;
-		}
+		MediaManager.stop();
 	};
 
 	$scope.playTrack = function(index) {
-		NextIndex = 0; // 재생 인덱스 초기화
-
 		$scope.dynamicTrack = $scope.tracks[index];  // 재생
 		$scope.togglePlayback = !$scope.togglePlayback;
 	};
-
-	$scope.playTracks = function(){
-		NextIndex = 0; // 재생 인덱스 초기화
-
-		$scope.playTrack(NextIndex); // 재생
-		if(($scope.checkTrack.length -1) > NextIndex) NextIndex++;
-		else NextIndex = 0;
-	};
-
-	$scope.toggleSelection = function toggleSelection(seq){
-		var idx = $scope.checkTrack.indexOf(seq);
-		if(idx > -1) $scope.checkTrack.splice(idx, 1);
-		else $scope.checkTrack.push(seq);
-	}
 }]);
