24298dc4857ffdb2f6637e90183e26b8d6af4ed1
yoonjihwan <gm1602759@gabiasafe.com>
2016-12-20 18:39:07 +0900

    데이터 가져오기 추가작업
    #서버개발 데이터추가작업

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,19 @@
 angular.module('starter.controllers', ['ionic', 'ionic-audio'])
-.controller('usCtrl', function($scope, $http) {
-	$http({
-	method: 'GET', //방식
-	url: 'http://masterplayer.net/music/main/album', /* 통신할 URL */
-	data: {seqno : 2} /* 파라메터로 보낼 데이터 */
-	})
-	.success(
-		function(res){
-			playlist = res.album;
-			console.log(playlist);
-		}
-	);
-})
+
+.controller('usCtrl', ['$scope', 'TrackList', function($scope, TrackList) {
+	TrackList.init(); // 초기화
+	TrackList.getMyPlaylist(); // 플레이리스트 가져오기
+}])
+
 .controller('searchCtrl', function($scope) {
 
 })
+
 .controller('topCtrl', function($scope) {
 
 })
-.controller('playerCtrl', ['$scope', 'MediaManager', 'TrackList', function($scope, MediaManager, TrackList) {
 
+.controller('playerCtrl', ['$scope', 'MediaManager', 'TrackList', function($scope, MediaManager, TrackList) {
 	var NextIndex = 0;
 	$scope.dynamicTrack = {};
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,10 @@
 angular.module('starter.services', [])
 
-.factory('TrackList', function() {
+.factory('TrackList', function($http) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var playlist = [];
+  var recommend = [];
   var tracks = [
     {
 	  seq: 0,
@@ -25,12 +25,52 @@ angular.module('starter.services', [])
       title: '난 아직 모르잖아요'
     }
   ];
+  var getList = function(seqno){
+	if(typeof seqno == 'undefined') seqno = 2; // Default Value
+	return $http({
+		method: 'GET', //방식
+		url: 'http://masterplayer.net/music/main/playlist', 
+		data: {seqno : seqno}
+		})
+		.success(function(res){
+			tracks = new Array();
+			 // Push Tracks
+			angular.forEach(res.album, function(t){
+				var track = {seq: t.SEQ, url: t.TRACK_STREAM, artist: t.ARTIST, title: t.TITLE};
+				tracks.push(track);
+			});
+			console.log("Status : Music PlayList Loaded");
+		}
+	);
+  }
+	
+  var getRecommend= function(){
+	return $http({
+		method: 'GET', //방식
+		url: 'http://masterplayer.net/music/main/recAlbum',
+		})
+		.success(function(res){
+			recommend = res.album;
+			console.log("Status : Recommend List Loaded");
+		}
+	);
+  }
+
   return {
+	init: function(){
+		getRecommend();
+	},
     all: function() {
       return tracks;
     },
 	setTrack: function(t){
 		playlist = t;
+	},
+	getMyPlaylist: function(seqno){
+		return getList(seqno);
+	},
+	getRecommandAlbum: function(){
+		return recommand;
 	}
   };
 });
