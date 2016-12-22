b86ab5fc1efac5c62fc355aa4f5f7aab84454f24
yoonjihwan <sac5@naver.com>
2016-12-23 01:39:48 +0900

    주요 레이아웃 화면 작업
    #4 레이아웃

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,30 +1,45 @@
 angular.module('starter.controllers', ['ionic', 'ionic-audio'])
 
-.controller('usCtrl', ['$scope', 'TrackList', function($scope, TrackList) {
-	TrackList.init(); // 초기화
-	TrackList.getMyPlaylist(); // 플레이리스트 가져오기
-}])
+  .controller('usCtrl', ['$scope', 'TrackList', function ($scope, TrackList) {
+    TrackList.init(); // 초기화
+    TrackList.getMyPlaylist(); // 플레이리스트 가져오기
+    TrackList.getRecommendAlbum();
+  }])
 
-.controller('searchCtrl', function($scope) {
+  .controller('searchCtrl', function ($scope) {
 
-})
+  })
 
-.controller('topCtrl', function($scope) {
+  // Index Page
+  .controller('topCtrl', ['$scope', 'TrackList', function ($scope, TrackList) {
+    TrackList.init(); // 초기화
+  }])
 
-})
-
-.controller('playerCtrl', ['$scope', 'MediaManager', 'TrackList', function($scope, MediaManager, TrackList) {
-	var NextIndex = 0;
-	$scope.dynamicTrack = {};
-
-	$scope.tracks = TrackList.all();
-
-	$scope.stopPlayback = function() {
-		MediaManager.stop();
-	};
-
-	$scope.playTrack = function(index) {
-		$scope.dynamicTrack = $scope.tracks[index];  // 재생
-		$scope.togglePlayback = !$scope.togglePlayback;
-	};
-}]);
+  // Player
+  .controller('playerCtrl', ['$scope', 'MediaManager', 'TrackList', '$timeout', function ($scope, MediaManager, TrackList, $timeout) {
+    var setPlayer = function () {
+      // Async
+      $timeout(function () {
+        if (TrackList.all().length > 0) {
+          $scope.dynamicTrack = {};
+          $scope.tracks = TrackList.all();
+          $scope.dynamicTrack = $scope.tracks[0];
+          console.log($scope.dynamicTrack);
+          /*
+          $scope.stopPlayback = function () {
+            MediaManager.stop();
+          };
+          $scope.playTrack = function (index) {
+            $scope.dynamicTrack = $scope.tracks[index];  // 재생
+            $scope.togglePlayback = !$scope.togglePlayback;
+          };
+          */
+        } else {
+          setPlayer();
+        }
+      });
+    }
+    // init
+    TrackList.getMyPlaylist(2);
+    setPlayer();
+  }]);
--- www/js/services.js
+++ www/js/services.js
@@ -5,72 +5,51 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var recommend = [];
-  var tracks = [
-    {
-	  seq: 0,
-      url: 'http://masterplayer.net/track/100000.mp3',  // audio file from the cloud
-      artist: '이문세',
-      title: '광화문연가'
-    },
-    {
-	  seq: 1,
-      url: 'http://masterplayer.net/track/100001.mp3',  // audio file from the cloud
-      artist: '이문세',
-      title: '그녀의 웃음소리뿐'
-    },
-    {
-	  seq: 2,
-      url: 'http://masterplayer.net/track/100002.mp3',  // audio file from the cloud
-      artist: '이문세',
-      title: '난 아직 모르잖아요'
-    }
-  ];
+  var tracks = [];
   var getList = function(seqno){
-	if(typeof seqno == 'undefined') seqno = 2; // Default Value
-	return $http({
-		method: 'GET', //방식
-		url: 'http://masterplayer.net/music/main/playlist', 
-		data: {seqno : seqno}
-		})
-		.success(function(res){
-			tracks = new Array();
-			 // Push Tracks
-			angular.forEach(res.album, function(t){
-				var track = {seq: t.SEQ, url: t.TRACK_STREAM, artist: t.ARTIST, title: t.TITLE};
-				tracks.push(track);
-			});
-			console.log("Status : Music PlayList Loaded");
-		}
-	);
-  }
-	
+	    if(typeof seqno == 'undefined') seqno = 2; // Default Value
+      return $http({
+        method: 'GET', //방식
+        url: 'http://masterplayer.net/music/main/playlist',
+        data: {seqno : seqno}
+        })
+        .success(function(res){
+          tracks = new Array();
+           // Push Tracks
+          angular.forEach(res.album, function(t){
+            var track = {seq: t.SEQ, url: t.TRACK_STREAM, artist: t.ARTIST, title: t.TITLE, thumb: t.ALBUM_THUMB};
+            tracks.push(track);
+          });
+          console.log(tracks);
+        }
+      );
+  };
+
   var getRecommend= function(){
-	return $http({
-		method: 'GET', //방식
-		url: 'http://masterplayer.net/music/main/recAlbum',
-		})
-		.success(function(res){
-			recommend = res.album;
-			console.log("Status : Recommend List Loaded");
-		}
-	);
-  }
+    return $http({
+      method: 'GET', //방식
+      url: 'http://masterplayer.net/music/main/recAlbum',
+      })
+      .success(function(res){
+        console.log(res);
+        recommend = res.album;
+        console.log("Status : Recommend List Loaded");
+      }
+    );
+  };
 
   return {
-	init: function(){
-		getRecommend();
-	},
+    init: function(){
+      getRecommend();
+    },
     all: function() {
       return tracks;
     },
-	setTrack: function(t){
-		playlist = t;
-	},
-	getMyPlaylist: function(seqno){
-		return getList(seqno);
-	},
-	getRecommandAlbum: function(){
-		return recommand;
-	}
+    getMyPlaylist: function(seqno){
+      return getList(seqno);
+    },
+    getRecommendAlbum: function(){
+      return getRecommend();
+    }
   };
 });
