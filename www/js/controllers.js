bf68c1e0607e0090ec9cb94616607081187af7a0
yoonjihwan <sac5@naver.com>
2016-12-30 02:59:21 +0900

    통합 작업 #1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,16 @@
-const groupseq = 1;
+var groupseq = 1;
+var cart = [];
+const user = "test";
 angular.module('starter.controllers', ['ionic'])
-
   .controller('usCtrl', ['$scope', '$state','TrackList', function ($scope, $state, TrackList) {
     $scope.trackSearch = function(){
       $state.go('search', { 'keyword': $scope.search });
     };    // 목록 담기
 
   }])
-
-  .controller('editCtrl', ['$scope','$stateParams', 'TrackList', function ($scope, $stateParams, TrackList) {
+  .controller('editCtrl', ['$scope','$stateParams', '$state', 'TrackList', function ($scope, $stateParams, $state, TrackList) {
     $scope.selection = [];
-    TrackList.getMyPlaylist(groupseq).then(function(d){
+    TrackList.getMyPlaylist(groupseq, user).then(function(d){
       var edit = [];
       angular.forEach(d.data.album, function (t) {
         var track = {seq: t.SEQ, url: t.TRACK_STREAM, artist: t.ARTIST, title: t.TITLE, thumb: t.ALBUM_THUMB, number: t.TRACK_NUMBER};
@@ -18,6 +18,10 @@ angular.module('starter.controllers', ['ionic'])
       });
       $scope.searchTracks = edit;
     });
+
+    $scope.doTheBack = function() {
+      window.history.back();
+    };
 
     // 토클된 트랙
     $scope.checkedItem = function(i){
@@ -39,10 +43,11 @@ angular.module('starter.controllers', ['ionic'])
       else {
         $scope.selection.push(index);
       }
+      cart = $scope.selection;
     };
-    
+
     $scope.setChangePost = function(){
-      TrackList.updatePlayGroup(groupseq, $scope.selection, "POST");
+      $state.go('send', null);
     };
   }])
 
@@ -99,42 +104,88 @@ angular.module('starter.controllers', ['ionic'])
   }])
 
   // Index Page
-  .controller('topCtrl', ['$scope', 'TrackList', function ($scope, TrackList) {
-  }])
-  // Myalbum
-  .controller('myalbumCtrl', ['$scope', 'TrackList', function ($scope, TrackList) {
+  .controller('topCtrl', ['$scope', 'TrackList', '$state', function ($scope, TrackList, $state) {
+    TrackList.init(); // 초기화
+    var albums = [{'PLAYGROUP' : groupseq, 'POST_TITLE' : '내가 담은 음악', 'POST_TOKEN' : null}];
+    TrackList.getAlbum(user).then(function(d){
+      angular.forEach(d.data[0], function(a){
+        albums.push(a);
+      });
+      $scope.albums = albums;
+    });
 
+    $scope.goAlbum = function(group){
+      if(typeof token == 'undefined'){
+        $state.go('player', { 'group': null });
+      } else {
+        $state.go('player', { 'group': group });
+      }
+    };    // 목록 담기
+  }])
+
+  // Myalbum
+  .controller('myalbumCtrl', ['$scope', 'TrackList', '$state', function ($scope, TrackList, $state) {
+    TrackList.init(); // 초기화
+    var albums = [];
+    TrackList.getAlbum(user).then(function(d){
+      angular.forEach(d.data[0], function(a){
+        albums.push(a);
+      });
+      $scope.albums = albums;
+    });
+
+    $scope.goAlbum = function(group){
+      if(typeof token == 'undefined'){
+        $state.go('player', { 'group': null });
+      } else {
+        $state.go('player', { 'group': group });
+      }
+    };    // 목록 담기
   }])
   // Send
   .controller('sendCtrl', ['$scope', 'TrackList', function ($scope, TrackList) {
 	  var token = "test";
 	  $scope.imageChange = function(){
-			console.log($scope.send.image);
+			console.log($scope.send);
 	  };
-	  $scope.send = function(){
-		  window.open('http://masterplayer.net/music/main/message?token=' + token, '_system');
+    $scope.albumcnt = cart.length;
+	  $scope.sender = function(){
+      console.log($scope.send);
+      TrackList.sendPost(groupseq, $scope.selection, null , "POST");
+		  window.open('http://masterplayer.net/main/message?token=' + token, '_system');
 	  };
   }])
   // Player
-  .controller('playerCtrl', ['$scope', 'TrackList', '$timeout', '$interval', 'PlayerService', function ($scope, TrackList, $timeout, $interval, PlayerService) {
+  .controller('playerCtrl', ['$scope', 'TrackList', '$timeout', '$interval', 'PlayerService', '$stateParams', function ($scope, TrackList, $timeout, $interval, PlayerService, $stateParams) {
 	var current = -1;
 	$scope.title = "No Track";
 	$scope.artist = "No Artist";
-	$scope.thumb = "";
-
+	$scope.thumb = ""
+  $scope.seek = 0;
+  $scope.progress = function(t){
+    var duration = PlayerService.duration();
+    var seek = Math.floor(duration*(t/100));
+    PlayerService.toSeek(seek);
+  };
 	$scope.playerPrev = function(){
 		PlayerService.previous();
+    $scope.seek = 0;
 	};
 	$scope.playerNext = function(){
 		PlayerService.next();
+    $scope.seek = 0;
 	};
 	$scope.playerStop = function(){
 		PlayerService.pause();
 	};
-	$scope.playerToPlay = function(i){
-		PlayerService.currentIndex = i;
-		PlayerService.play();
-	}
+    $scope.playerToPlay = function (i) {
+      PlayerService.currentIndex = i;
+      PlayerService.play();
+    };
+    $scope.formatDate = function (date) {
+      var dateOut = new Date(date);
+      return dateOut;
+    };
 	var setPlayer = function () {
 	  // Async
 	  $timeout(function () {
@@ -158,13 +209,18 @@ angular.module('starter.controllers', ['ionic'])
 			$scope.title = PlayerService.trackList[index].title;
 			$scope.artist = PlayerService.trackList[index].artist;
 			$scope.thumb = PlayerService.trackList[index].thumb;
+      $scope.indexer = index;
 			current = index;
 		}
-		var seek = PlayerService.time();
-		var duration = PlayerService.duration();
-		console.log(Math.floor(seek) + "/" + Math.floor(duration)) ;
-	}, 1000);
-	// init
-	setPlayer();
-	TrackList.init(); // 초기화
+    var duration = PlayerService.duration();
+    var time = PlayerService.time();
+    $scope.seek = Math.floor(100*(time/duration));
+    $scope.playtime = Math.floor(time*1000);
+    $scope.endtime = Math.floor(duration*1000);
+	}, 500);
+    // init
+    if(typeof $stateParams.group == 'undefined') $stateParams.group = groupseq;
+    else groupseq = $stateParams.group;
+    TrackList.getMyPlaylist($stateParams.group, user);
+    setPlayer();
   }]);
--- www/js/services.js
+++ www/js/services.js
@@ -1,147 +1,163 @@
 angular.module('starter.services', [])
-.factory('TrackList', function($http, $timeout) {
-  // Might use a resource here that returns a JSON array
+  .factory('TrackList', function($http, $timeout) {
+    // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var recommend = [];
-  var tracks = [];
-  var search = [];
-  var getList = function(seqno, type){
-	  if(typeof seqno == 'undefined') seqno = 2; // Default Value
+    // Some fake testing data
+    var recommend = [];
+    var tracks = [];
+    var search = [];
+    var getList = function(seqno, user){
       return $http({
         method: 'GET', //방식
-        url: "http://masterplayer.net/music/main/playlist?seqno=" + seqno + "&type=" + type
-      });
-  };
-  var getRecommend= function(){
-    return $http({
-      method: 'GET', //방식
-      url: 'http://masterplayer.net/music/main/recAlbum',
+        url: "http://masterplayer.net/main/playlist?seqno=" + seqno + "&user=" + user
       })
-      .success(function(res){
-        recommend = res.album;
-        console.log("Status : Recommend List Loaded");
-      }
-    );
-  };
+        .success(function(res){
+            tracks = new Array();
+            // Push Tracks
+            angular.forEach(res.album, function(t){
+              var track = {seq: t.SEQ, url: t.TRACK_STREAM, artist: t.ARTIST, title: t.TITLE, thumb: t.ALBUM_THUMB, send: t.TYPE};
+              tracks.push(track);
+            });
+          }
+        );
+    };
+    var getRecommend= function(){
+      return $http({
+        method: 'GET', //방식
+        url: 'http://masterplayer.net/main/recAlbum',
+      })
+        .success(function(res){
+            recommend = res.album;
+            console.log("Status : Recommend List Loaded");
+          }
+        );
+    };
 
-  return {
-    init: function(){
-		tracks = [];
-		getRecommend();
-		getList(groupseq, "PLAYLIST").then(function(d){
-			receive = [];
-			angular.forEach(d.data.album, function (t) {
-				var track = {seq: t.SEQ, url: t.TRACK_STREAM, artist: t.ARTIST, title: t.TITLE, thumb: t.ALBUM_THUMB, number: t.TRACK_NUMBER};
-				receive.push(track);
-			});
-			tracks = receive;
-		});
-    },
-    all: function() {
-      return tracks;
-    },
-	setTrack: function(t) {
-		tracks = t;
-	},
-    getMyPlaylist: function(seqno){
-      return getList(seqno, "PLAYLIST");
-    },
-    getEditlist: function(seqno){
-      return getList(seqno, "POST");
-    },
-    getRecommendAlbum: function(){
-      return getRecommend();
-    },
-    getSearchTrack : function(text){
-      return $http({
-        method: 'get', //방식
-        url: 'http://masterplayer.net/music/main/search?keyword=' + text,
-        data: {keyword: text}
-      });
-    },
-    pushPlayGroup : function(group, tracks, type){
-      return $http({
-        method: 'post', //방식
-        url: 'http://masterplayer.net/music/main/push',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        transformRequest: function(obj) {
-          var str = [];
-          for(var p in obj)
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
-        },
-        data : {group: group, tracks: tracks, type: type}
-      }).success(function(d){
+    return {
+      init: function(){
+        tracks = [];
+        getRecommend();
+        getList(groupseq, "PLAYLIST").then(function(d){
+          receive = [];
+          angular.forEach(d.data.album, function (t) {
+            var track = {seq: t.SEQ, url: t.TRACK_STREAM, artist: t.ARTIST, title: t.TITLE, thumb: t.ALBUM_THUMB, number: t.TRACK_NUMBER};
+            receive.push(track);
+          });
+          tracks = receive;
+        });
+      },
+      all: function() {
+        return tracks;
+      },
+      getMyPlaylist: function(seqno, user){
+        return getList(seqno, user);
+      },
+      getEditlist: function(seqno){
+        return getList(seqno, "POST");
+      },
+      getRecommendAlbum: function(){
+        return getRecommend();
+      },
+      getAlbum : function(user){
+        return $http({
+          method: 'get', //방식
+          url: 'http://masterplayer.net/main/AlbumList?user=' + user
+        });
+      },
+      getSearchTrack : function(text){
+        return $http({
+          method: 'get', //방식
+          url: 'http://masterplayer.net/main/search?keyword=' + text,
+          data: {keyword: text}
+        });
+      },
+      pushPlayGroup : function(group, tracks, type){
+        return $http({
+          method: 'post', //방식
+          url: 'http://masterplayer.net/main/push',
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+          transformRequest: function(obj) {
+            var str = [];
+            for(var p in obj)
+              str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            return str.join("&");
+          },
+          data : {group: group, tracks: tracks, type: type}
+        }).success(function(d){
+          console.log(d);
+        });
+      },
+      sendPost : function(group, tracks, type, d){
         console.log(d);
-      });
-    },
-    updatePlayGroup : function(group, tracks, type){
-      return $http({
-        method: 'post', //방식
-        url: 'http://masterplayer.net/music/main/updatepost',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        transformRequest: function(obj) {
-          var str = [];
-          for(var p in obj)
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
-        },
-        data : {group: group, tracks: tracks, type: type}
-      }).success(function(d){
-        console.log(d);
-      });
-    }
-  };
-})
-.service('PlayerService', function(AudioFactory) {
-  var myPlayer = {
-    
-    isPaused: true,
-    // Sample tracks, to be replaced by the audio files you actually want to use
-    trackList: [],
-    currentIndex: 0,
-	currentTime: 0,
-    play: function () {
-                myPlayer.isPaused = false;
-                AudioFactory.src = myPlayer.trackList[myPlayer.currentIndex].url;
-                AudioFactory.play();
-				myPlayer.currentTime = 0;
-            },
-    pause: function () {
-                myPlayer.isPaused = !myPlayer.isPaused;
-                if (myPlayer.isPaused) {
-                    AudioFactory.pause();
-					myPlayer.currentTime = AudioFactory.currentTime;
-                } else {
-					AudioFactory.currentTime = myPlayer.currentTime;
-                    AudioFactory.play();
-                }
-            },
-    previous: function () {
-                if (myPlayer.currentIndex > 0) {
-                    myPlayer.currentIndex--;
-                    myPlayer.play();
-                }
-            },
- 
-    next: function () {
-                if (myPlayer.currentIndex < myPlayer.trackList.length) {
-                    myPlayer.currentIndex++;
-                    myPlayer.play();
-                }
-            },
-	duration : function(){
-		return AudioFactory.duration;
-	},
-	time : function(){
-		myPlayer.currentTime = AudioFactory.currentTime;
-		return AudioFactory.currentTime;
-	}
-  };
-   
-  return myPlayer;
-})
+        return $http({
+          method: 'post', //방식
+          url: 'http://masterplayer.net/main/updatepost',
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+          transformRequest: function(obj) {
+            var str = [];
+            for(var p in obj)
+              str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            return str.join("&");
+          },
+          data : {group: group, tracks: tracks, type: type}
+        }).success(function(d){
+          console.log(d);
+        });
+      }
+    };
+  })
+  .service('PlayerService', function(AudioFactory) {
+    var myPlayer = {
+
+      isPaused: true,
+      // Sample tracks, to be replaced by the audio files you actually want to use
+      trackList: [],
+      currentIndex: 0,
+      currentTime: 0,
+      play: function () {
+        myPlayer.isPaused = false;
+        AudioFactory.src = myPlayer.trackList[myPlayer.currentIndex].url;
+        AudioFactory.play();
+        myPlayer.currentTime = 0;
+      },
+      pause: function () {
+        myPlayer.isPaused = !myPlayer.isPaused;
+        if (myPlayer.isPaused) {
+          AudioFactory.pause();
+          myPlayer.currentTime = AudioFactory.currentTime;
+        } else {
+          AudioFactory.currentTime = myPlayer.currentTime;
+          AudioFactory.play();
+        }
+      },
+      previous: function () {
+        if (myPlayer.currentIndex > 0) {
+          myPlayer.currentIndex--;
+          myPlayer.play();
+        }
+      },
+
+      next: function () {
+        if (myPlayer.currentIndex < myPlayer.trackList.length) {
+          myPlayer.currentIndex++;
+          myPlayer.play();
+        }
+      },
+      duration : function(){
+        return AudioFactory.duration;
+      },
+      time : function(){
+        myPlayer.currentTime = AudioFactory.currentTime;
+        return AudioFactory.currentTime;
+      },
+      toSeek : function(t){
+        AudioFactory.currentTime = t;
+      }
+    };
+
+    return myPlayer;
+  })
+
 .factory('AudioFactory', function($document) {
   var audio = $document[0].createElement('audio');
   return audio;
